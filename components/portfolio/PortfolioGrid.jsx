505a9dac517658c529e9aa2660390bfd11a763f5
Tam Jid <tamjid.mdh@gmail.com>
2022-09-04 11:28:34 +0600

    console log in dev mode

--- components/portfolio/PortfolioGrid.jsx
+++ components/portfolio/PortfolioGrid.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Image from 'next/image';
 
 const PortfolioGrid = ({ data }) => {
-    console.log(data);
+    if (process.env.NODE_ENV === 'development') {
+        console.log(data);
+    }
     return (
         <section className="bg-gray-100 py-10 px-12">
             <div className="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
