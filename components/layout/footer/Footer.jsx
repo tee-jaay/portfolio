8e3adceca1f2d645b3906b55d6d29f9e98d32efb
Tam Jid <tamjid.mdh@gmail.com>
2022-09-05 13:22:42 +0600

    portfolio items to scrollable x

--- components/layout/footer/Footer.jsx
+++ components/layout/footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Footer = () => {
+    let year = new Date().getFullYear();
     return (
-        <footer className="py-4">
-            <span font-quicksand>tamjid &copy; 2022</span>
+        <footer className="mt-4 py-4">
+            <span font-quicksand className='text-xs'>tamjid &copy; {year}</span>
         </footer>
     );
 };
--- components/layout/header/Header.jsx
+++ components/layout/header/Header.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className="header py-8">
-            <h2 className="text-center font-bold text-4xl font-quicksand">
+        <div className="header my-8 py-8">
+            <h2 className="text-center font-bold leading-12 text-4xl font-quicksand">
                 Fullstack Developer
             </h2>
 
-            <p className="sub-header leading-9 text-2xl font-quicksand">
+            <p className="sub-header text-center leading-8 text-2xl font-quicksand">
                 I am a software developer with a passion for building web & mobile applications
             </p>
         </div>
