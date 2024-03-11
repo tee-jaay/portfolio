5aa696d97a8a2e917979379f9e67341005ca0e7e
Tee Jaay <tamjid84@yahoo.com>
2024-03-11 18:20:26 +0600

    yarn upgrade

--- components/layout/footer/Footer.jsx
+++ components/layout/footer/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     let year = new Date().getFullYear();
     return (
         <div className='pb-4'>
-            <h6 className="text-xs text-center font-quicksand">{process.env.COPYRIGHT_TEXT ? process.env.COPYRIGHT_TEXT : 'tamjid'} &copy; {year}</h6>
+            <h6 className="text-xs text-center font-quicksand">Copyright &copy; {year}. <span className="font-bold">{process.env.COPYRIGHT_TEXT ? process.env.COPYRIGHT_TEXT : 'tamjid'}</span> All Rights Reserved.</h6>
         </div>
     );
 };
